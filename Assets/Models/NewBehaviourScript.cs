84a11d3ec25a279ea938da96b3d0c2f91f8feba8
Perl.oG <perepelytsagleb2@gmail.com>
2023-08-02 11:28:11 +0300

    link

--- Assets/Models/NewBehaviourScript.cs
+++ Assets/Models/NewBehaviourScript.cs
@@ -1,3 +1,25 @@
-/*https://www.turbosquid.com/3d-models/book-collection-of-ivan-franko-works-3d-model-2036784
+/*
+https://www.turbosquid.com/3d-models/book-collection-of-ivan-franko-works-3d-model-2036784
+https://www.turbosquid.com/3d-models/book-collection-of-ivan-franko-works-3d-model-2036784
+https://www.turbosquid.com/3d-models/3d-model-table-lamp-1927025
+https://skfb.ly/oDAYx - надо
+https://skfb.ly/oBCpD
+https://skfb.ly/6WP6v
+https://skfb.ly/6THuz - надо
+https://skfb.ly/6WNHI
+https://skfb.ly/owYHM
+https://skfb.ly/Y9sx
+https://skfb.ly/6tzJS
+https://skfb.ly/6BYHM
+https://skfb.ly/6xPWF - надо
+https://skfb.ly/6RUWv
+https://skfb.ly/6XN6V
+https://skfb.ly/ozMK8
+https://skfb.ly/6pMGN
+https://skfb.ly/6VFGZ
+https://skfb.ly/oCGwP
+https://skfb.ly/6wyVD
+https://skfb.ly/6Dw7W
+https://skfb.ly/6zMSS - надо
 
 */
