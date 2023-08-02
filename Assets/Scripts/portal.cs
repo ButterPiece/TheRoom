351aabaa84275e9491b2624a5cc43d8fce6ec9df
zak10110 <35228898+zak10110@users.noreply.github.com>
2023-08-02 15:59:16 +0300

    Portal Fix

--- Assets/Scripts/portal.cs
+++ Assets/Scripts/portal.cs
@@ -7,8 +7,9 @@ public class portal : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        other.transform.position = _outPort.position;
+        GameObject.FindGameObjectWithTag("Player").transform.position= _outPort.position;
+        //other.transform.position = _outPort.position;
         float rotY = _outPort.rotation.eulerAngles.y;
-        other.transform.rotation = Quaternion.Euler(other.transform.rotation.x, rotY - 100, other.transform.rotation.z);
+        GameObject.FindGameObjectWithTag("Player").transform.rotation = Quaternion.Euler(other.transform.rotation.x, rotY - 100, other.transform.rotation.z);
     }
 }
